c3d5844b39e6b7a25751febe926d82315f55f24e
alekslesik <alekslesik@gmail.com>
2023-06-19 18:02:38 +0300

    add moc for test tokens

--- server/http/auth/mock.go
+++ server/http/auth/mock.go
@@ -1,1 +1,24 @@
 package auth
+
+import "time"
+
+type MockTokenManager struct {
+	ReturnInvalidToken bool
+	ReturnExpiredToken bool
+}
+
+func (m *MockTokenManager) CreateToken(username string, duration time.Duration) (string, *PasetoPayload, error) {
+	return "testtoken", &PasetoPayload{}, nil
+}
+
+func (m *MockTokenManager) VerifyToken(token string) (*PasetoPayload, error) {
+	if m.ReturnExpiredToken {
+		return nil, ErrTokenExpired
+	}
+
+	if m.ReturnInvalidToken {
+		return nil, ErrTokenInvalid
+	}
+
+	return &PasetoPayload{}, nil
+}
